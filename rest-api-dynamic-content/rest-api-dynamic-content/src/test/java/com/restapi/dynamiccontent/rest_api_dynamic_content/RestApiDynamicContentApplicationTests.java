9c390a14a796ff502efb7c0491e9002bab2afeb5
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2025-02-02 01:37:58 +0530

    setup project

---
+++ rest-api-dynamic-content/rest-api-dynamic-content/src/test/java/com/restapi/dynamiccontent/rest_api_dynamic_content/RestApiDynamicContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.dynamiccontent.rest_api_dynamic_content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDynamicContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
