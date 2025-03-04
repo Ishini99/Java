819a505ac2c008574bd2f488472302769da457f8
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2025-03-04 13:16:48 +0530

    updated inject properties from application. properties file

--- Springboot/demo/demo/src/main/java/com/springDemo/demo/restControlers/restController.java
+++ Springboot/demo/demo/src/main/java/com/springDemo/demo/restControlers/restController.java
@@ -1,5 +1,6 @@
 package com.springDemo.demo.restControlers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +14,16 @@ public class restController {
     public String sayHello2(){
         return "hello test2";
     }
+    //inject properties from properties file
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+//expose new endpoint for team info
+    @GetMapping("/teamInfo")
+    public String teamInfo(){
+        return "Coach name:  " + coachName +"  Team name: "+teamName;
+    }
+
+
 }
